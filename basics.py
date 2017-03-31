ca221ca3959d041f00b343c4c108ceacb68af033
TomHarr1s <thomasharris2001@hotmail.co.uk>
2017-03-31 13:46:19 +0100

    Create basics.py
    Basics

---
+++ basics.py
@@ -0,0 +1,16 @@
+#To start of with you need to learn how to print text you would do this by:
+
+print ("Hello world")
+
+#Under this make it print Hello my name is Myles
+
+
+#Next you need to learn how to instert variables you would do this by:
+
+my_variable = 10
+
+#This would assign the value "10" the the variable "my_variable"
+#You can then make it print the variable by:
+
+my_variable = 10
+print (my_variable)
